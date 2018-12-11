971d2fe1769a988f7c752efc846e1eeb4fecba24
jefflee168 <86116193@qq.com>
2018-12-11 08:47:07 -0500

    first commit

---
+++ HelloWorld/testdb.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+from django.http import HttpResponse
+
+from TestModel.models import Test
+
+def testdb(request):
+    test1 = Test(name='jeff')
+    test1.save()
+    return HttpResponse("<p>数据添加成功！</p>")
